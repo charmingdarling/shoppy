fbd50998acffd1155b7b5adf3a3a492fb786b939
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-12 02:45:28 -0700

    Added apiRoutes notes.

--- .notes/routes_index_notes.js
+++ .notes/routes_index_notes.js
@@ -3,6 +3,10 @@ const apiRoutes = require("./api");
 router.use("api", apiRoutes);
 module.exports = router;
 
+// ---------------------------------------------------------- //
+// ---------------------------------------------------------- //
+// ---------------------------------------------------------- //
+// * What is ...? //
 const router = require("express").Router();
 
 // Setting up an instance of an Express router. In Express.js, routers are used to create modular, mountable route handlers. They allow you to group related routes and handlers together, making your code more organized and maintainable.
@@ -52,3 +56,76 @@ app.listen(port, () => {
 });
 
 // This modular approach helps to organize your code, especially as your application grows and you have multiple routes. Each module (router) can handle a specific set of related routes, improving code readability and maintainability.
+
+// * End
+
+// ---------------------------------------------------------- //
+// ---------------------------------------------------------- //
+// ---------------------------------------------------------- //
+
+// * What is ...? //
+
+const apiRoutes = require("./api");
+// - is importing a module or file named "api" and assigning it to the variable `apiRoutes`.
+
+// **`require("./api")`**
+// - This statement is using the `require` function in Node.js to import the contents of a file or module.
+// - The argument passed to `require` is a relative path to the file or module.
+
+// ? NOTE: (relative path) -
+// A relative path is a file or directory path that is relative to the current working directory or another specified directory. It specifies the location of a file or directory relative to the current location of the program or file that is referencing it.
+// Relative paths do not start with a forward slash (/) or a drive letter, and they can include relative directory references such as ".." (parent directory) or "." (current directory).
+
+// ? Note: (absolute path) -
+// An absolute path is a file or directory path that specifies the exact location of a file or directory in the file system. It provides the full path from the root directory to the file or directory.
+// Absolute paths typically start with a forward slash (/) on Unix-like systems or with a drive letter (such as C:) on Windows systems.
+// Absolute paths do not depend on the current working directory and can be used to access files or directories from any location in the file system.
+
+// **`const apiRoutes = ...`**
+// - The result of the `require("./api")` statement is assigned to the variable `apiRoutes`.
+// - This means that whatever is exported from the "api" module becomes accessible through the `apiRoutes` variable in the current module.
+
+// Here's an example of how this might work:
+
+// * Assuming you have a file named "api.js" in the same directory:
+
+// api.js
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/users", (req, res) => {
+  res.json({ message: "API endpoint for users" });
+});
+
+router.get("/posts", (req, res) => {
+  res.json({ message: "API endpoint for posts" });
+});
+
+module.exports = router;
+
+// * Now, in another file (let's call it "app.js" for simplicity):
+
+// * app.js
+
+const express = require("express");
+const app = express();
+
+// Import the apiRoutes module
+const apiRoutes = require("./api");
+
+// Use the imported routes at a specific path
+app.use("/api", apiRoutes);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// In this example:
+
+// - The `require('./api')` statement in "app.js" imports the router from the "api.js" file.
+// - The `apiRoutes` variable now contains the router with the defined routes for `/users` and `/posts`.
+// - `app.use('/api', apiRoutes);` tells Express to use the routes defined in `apiRoutes` at the path `/api`. So, accessing `/api/users` and `/api/posts` will trigger the corresponding route handlers from the "api.js" module.
+
+// This modular approach helps organize your code by breaking it into smaller, manageable pieces. Each module can handle specific functionality, making your application more maintainable and easier to understand.
