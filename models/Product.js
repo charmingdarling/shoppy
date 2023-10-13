2928f1e24890b0c4afaaf0566136b696dfb5d80d
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-12 21:53:13 -0700

    Defining Product model (id, product_name, price, stock, category_id)

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,5 @@
 // Import important parts of sequelize library
-const { Model, Datatypes } = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
 // Import database connection from config.js
 const sequelize = require("..config/connection.js");
 // Initialize Product model (table) by extending off Sequelize's Model class
@@ -8,7 +8,39 @@ class Product extends Model {}
 // Set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(6, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
