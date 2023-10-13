20f54a281ae77b3f72863376f158a6ecd75caf5c
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-12 21:57:56 -0700

    ProductTag columns defined. (id, product_id, tag_id)

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,10 +2,37 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 class ProductTag extends Model {}
 
+// Many to Many relationship
+// Many product with many tags
+// Many tag with many products
+// - ie: A black tag with many types of products (shirts, shoes, socks)
+
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "tag",
+        key: "id",
+      },
+    },
   },
+
   {
     sequelize,
     timestamps: false,
